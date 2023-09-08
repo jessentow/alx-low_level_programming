8955527af7fce6e2f050daac3b6df1602a663208
Jesse Ntow <jessentow@gmail.com>
2023-09-07 21:08:46 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A program that prints exactly with proper grammar
+ *
+ * Return:0(successful)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
