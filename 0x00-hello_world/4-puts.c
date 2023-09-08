0ec82851c4c02a6b8c4184ddf3ad63442818ab95
Jesse Ntow <jessentow@gmail.com>
2023-09-07 20:30:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A program that prints lines with the put function
+ * Return : 0 (when the program returns 0, then we are successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
