c943bea808bede59e9ad64e678a0d6fb7a6182bc
Jesse Ntow <jessentow@gmail.com>
2023-10-09 22:44:27 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,21 +3,22 @@
 #include <stdio.h>
 
 /*
- * struct dog: Represents information about a dog
+ * struct dog - Represents information about a dog
  * @name: The name of the dog
  * @age: The age of the dog
  * @owner: The owner of the dog
- *
- * Description: This structure defines the elements of a dog, including its
- * name, age, and owner. It is used to represent the struct dog
  */
 
-void main(struct dog);
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/*
+ * dog_t - typedef for struct dog
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
