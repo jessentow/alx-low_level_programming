6ffb1a55cc2c9ed76e4140f428f95a6679f92943
Jesse Ntow <jessentow@gmail.com>
2023-10-20 22:37:08 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,8 @@
+#include <stdlib.h>
 #include <string.h>
 #include "lists.h"
+#include <stddef.h>
+
 
 /**
  * add_node - This will add a new node at the beginning of
@@ -9,21 +12,22 @@
  *
  * Return: the address of the new element, or NULL if it failed.
  */
-
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str);
 {
-	list_t *new_node;
-	unsigned int c = 0;
+	list_t *new;
+	unsigned int a = 0;
 
-	while
-	(str[c++]);
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	while (str[len])
+	len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
 	return (NULL);
 
-	new_node->str = strdup(str);
-	new_node->len = --c;
-	new_node->next = (*head);
-	(*head) = new_node;
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
 	return (*head);
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,2 +1,47 @@
 #include <string.h>
 #include "lists.h"
+
+/**
+ * add_node_end - This will add a new node at the end of
+ * a singly linked list.
+ * @head: This shows a linked list to print
+ * @str: this is a need to be duplicated.
+ *
+ * Return: This will return the address of the new element,
+ * or NULL if it failed.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *tmp_node; /* Create a new node */
+	unsigned int a = 0;
+
+	if (str == NULL)
+	return (NULL);
+
+	while (str[a++])
+	;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = --a;
+	new_node->next = NULL;
+
+
+	if (*head == NULL)
+	{
+	(*head) = new_node;
+	return (new_node);
+	}
+
+	tmp_node = *head;
+	while (tmp_node->next)
+	tmp_node = tmp_node->next;
+
+	tmp_node->next = new_node;
+
+	return (new_node);
+}
