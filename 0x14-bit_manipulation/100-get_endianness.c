5403ee7dcc733b73a689f362a0aef0e12e227579
Jesse Ntow <jessentow@gmail.com>
2023-10-26 08:56:00 -0700

    a function that checks the endianness.

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * get_endianness - this will check for the endianness
+ *
+ * Return: it will return 0 - if big endian,
+ *         1 - if little endian
+ *
+ * Description: In big endian format the most significant byte
+ *	is stored earliest, thus gets stored at the
+ *	latest address byte, while in little endian format
+ *	the least significant byte is stored first.
+ */
+
+int get_endianness(void)
+{
+	unsigned int a = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
